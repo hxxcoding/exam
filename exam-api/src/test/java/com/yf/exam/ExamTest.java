f62f3cc90c643ef7e6f04d52d579a78fad8e36ea
hxx <hxx91213@gmail.com>
2021-12-30 22:46:02 +0800

    [Xiaoxiao Hu] feat: delete bigfile

--- exam-api/src/test/java/com/yf/exam/ExamTest.java
+++ exam-api/src/test/java/com/yf/exam/ExamTest.java
@@ -1,32 +1,33 @@
 package com.yf.exam;
 
-import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.yf.exam.core.api.dto.PagingReqDTO;
-import com.yf.exam.modules.exam.dto.ExamDTO;
-import com.yf.exam.modules.exam.dto.response.ExamOnlineRespDTO;
+
 import com.yf.exam.modules.exam.service.ExamService;
+import org.apache.poi.xwpf.model.XWPFHeaderFooterPolicy;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = ExamApplication.class)
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
+
 public class ExamTest {
 
     @Autowired
     private ExamService examService;
 
     @Test
-    public void testOnline(){
-        PagingReqDTO<ExamDTO> reqDTO = new PagingReqDTO();
-        reqDTO.setCurrent(1);
-        reqDTO.setSize(10);
-
-        System.out.println("++++++=开始查询：：：");
-        IPage<ExamOnlineRespDTO> data = examService.onlinePaging(reqDTO);
-
-        System.out.println("++++++=查询结果：：："+data);
+    public void testOnline() throws IOException {
+        InputStream is = new FileInputStream("/Users/hxx/Desktop/test.docx");
+        XWPFDocument docx = new XWPFDocument(is);
+        XWPFHeaderFooterPolicy headerFooterPolicy = docx.getHeaderFooterPolicy();
+        System.out.println(headerFooterPolicy.getDefaultFooter().getText());
     }
 }
